445d518e6e2ccbb1517259a239079ffa07eeb4a6
EvgeniaRepina <88374316+EvgeniaRepina@users.noreply.github.com>
2021-11-17 17:40:05 +0100

    broken Postman Echo

--- src/test/java/ru/netology/rest/PostmanEchoTest.java
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -20,6 +20,6 @@ class PostmanEchoTest {
                 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some data"));
+                .body("data", equalTo("somt data"));
     }
 }
